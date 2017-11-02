a3677429d75c3e92f513c388f4dfa0a5e45abccb
jonathan11ch <jonathan11ch@hotmail.com>
2017-11-02 15:44:56 +0000

    New Interface

--- gui/Interface.py
+++ gui/Interface.py
@@ -30,10 +30,10 @@ class MainTherapyWin(QtWidgets.QWidget):
         #Window title
         self.setWindowTitle("Lokomat therapy")
         #Window size
-        self.setGeometry(100,100,1000,600)#Tamaño de la ventana
+        self.setGeometry(100,100,1000,580)#Tamaño de la ventana
         #setting backgroung image
-        Image=QImage("gui/img/back1.jpg")
-        sImage=Image.scaled(QSize(1000,600))
+        Image=QImage("gui/img/Int.jpg")
+        sImage=Image.scaled(1000,700,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
         palette=QPalette()
         palette.setBrush(10, QBrush(sImage))
         self.setPalette(palette)
@@ -41,145 +41,106 @@ class MainTherapyWin(QtWidgets.QWidget):
         #-----------Labels config----------
         #Heart rate:
         self.hrDisplay = {}
-        #heart_rate label
-        self.hrDisplay['name'] = QtWidgets.QLabel(self)
-        self.hrDisplay['name'].setText("Heart Rate")
-        self.hrDisplay['name'].setStyleSheet("font-size:20px; Arial")
-        self.hrDisplay['name'].setGeometry(40,40, 100,100)
-        #heart rate units label
-        self.hrDisplay['units'] = QtWidgets.QLabel(self)
-        self.hrDisplay['units'].setText("Bpm")
-        self.hrDisplay['units'].setStyleSheet("font-size:20px; Arial")
-        self.hrDisplay['units'].setGeometry(280,40, 100,100)
         #heart rate lcd
         self.hrDisplay['lcd'] = QtWidgets.QLCDNumber(self)
-        self.hrDisplay['lcd'].setGeometry(150,65, 100,50)
+        self.hrDisplay['lcd'].setGeometry(120,95, 120,30)
         #Yaw angle:
         self.yawDisplay = {}
         #yaw angle label
         self.yawDisplay['name'] = QtWidgets.QLabel(self)
         self.yawDisplay['name'].setText("Yaw")
-        self.yawDisplay['name'].setStyleSheet("font-size:20px; Arial")
-        self.yawDisplay['name'].setGeometry(50,110, 100,100)
-        #yaw angle units label
-        self.yawDisplay['units'] = QtWidgets.QLabel(self)
-        self.yawDisplay['units'].setText("Deg")
-        self.yawDisplay['units'].setStyleSheet("font-size:20px; Arial")
-        self.yawDisplay['units'].setGeometry(280,110, 100,100)
+        self.yawDisplay['name'].setStyleSheet("font-size:15px; Arial")
+        self.yawDisplay['name'].setGeometry(120,125, 100,50)
         #yaw lcd
         self.yawDisplay['lcd'] = QtWidgets.QLCDNumber(self)
-        self.yawDisplay['lcd'].setGeometry(150,130, 100,50)
+        self.yawDisplay['lcd'].setGeometry(100,165, 50,30)
         #pitch angle:
         self.pitchDisplay = {}
         #pitch angle label
         self.pitchDisplay['name'] = QtWidgets.QLabel(self)
         self.pitchDisplay['name'].setText("Pitch")
-        self.pitchDisplay['name'].setStyleSheet("font-size:20px; Arial")
-        self.pitchDisplay['name'].setGeometry(50,180, 100,100)
-        #pitch angle units label
-        self.pitchDisplay['units'] = QtWidgets.QLabel(self)
-        self.pitchDisplay['units'].setText("Deg")
-        self.pitchDisplay['units'].setStyleSheet("font-size:20px; Arial")
-        self.pitchDisplay['units'].setGeometry(280,180, 100,100)
+        self.pitchDisplay['name'].setStyleSheet("font-size:15px; Arial")
+        self.pitchDisplay['name'].setGeometry(175,125, 100,50)
         #pitch lcd
         self.pitchDisplay['lcd'] = QtWidgets.QLCDNumber(self)
-        self.pitchDisplay['lcd'].setGeometry(150,200, 100,50)
+        self.pitchDisplay['lcd'].setGeometry(160,165, 50,30)
         #roll angle:
         self.rollDisplay = {}
         #roll angle label
         self.rollDisplay['name'] = QtWidgets.QLabel(self)
         self.rollDisplay['name'].setText("Roll")
-        self.rollDisplay['name'].setStyleSheet("font-size:20px; Arial")
-        self.rollDisplay['name'].setGeometry(50,250, 100,100)
-        #roll angle units label
-        self.rollDisplay['units'] = QtWidgets.QLabel(self)
-        self.rollDisplay['units'].setText("Deg")
-        self.rollDisplay['units'].setStyleSheet("font-size:20px; Arial")
-        self.rollDisplay['units'].setGeometry(280,250, 100,100)
+        self.rollDisplay['name'].setStyleSheet("font-size:15px; Arial")
+        self.rollDisplay['name'].setGeometry(230,125, 100,50)
         #roll lcd
         self.rollDisplay['lcd'] = QtWidgets.QLCDNumber(self)
-        self.rollDisplay['lcd'].setGeometry(150,270, 100,50)
+        self.rollDisplay['lcd'].setGeometry(220,165, 50,30)
+        #Therapy time lcd
+        self.TtimeDisplay = {}
+        self.TtimeDisplay['lcd'] = QtWidgets.QLCDNumber(self)
+        self.TtimeDisplay['lcd'].setGeometry(865,45, 100,50)
 
 
-
-        a =300
+        #a =300
         #Yaw angle:
         self.yawDisplay1 = {}
-        #yaw angle label
-        self.yawDisplay1['name'] = QtWidgets.QLabel(self)
-        self.yawDisplay1['name'].setText("Yaw")
-        self.yawDisplay1['name'].setStyleSheet("font-size:20px; Arial")
-        self.yawDisplay1['name'].setGeometry(50 + a,110, 100,100)
-        #yaw angle units label
-        self.yawDisplay1['units'] = QtWidgets.QLabel(self)
-        self.yawDisplay1['units'].setText("Deg")
-        self.yawDisplay1['units'].setStyleSheet("font-size:20px; Arial")
-        self.yawDisplay1['units'].setGeometry(280 + a,110, 100,100)
+
         #yaw lcd
         self.yawDisplay1['lcd'] = QtWidgets.QLCDNumber(self)
-        self.yawDisplay1['lcd'].setGeometry(150 + a,130, 100,50)
+        self.yawDisplay1['lcd'].setGeometry(100,235, 50,30)
         #pitch angle:
         self.pitchDisplay1 = {}
-        #pitch angle label
-        self.pitchDisplay1['name'] = QtWidgets.QLabel(self)
-        self.pitchDisplay1['name'].setText("Pitch")
-        self.pitchDisplay1['name'].setStyleSheet("font-size:20px; Arial")
-        self.pitchDisplay1['name'].setGeometry(50 + a,180, 100,100)
-        #pitch angle units label
-        self.pitchDisplay1['units'] = QtWidgets.QLabel(self)
-        self.pitchDisplay1['units'].setText("Deg")
-        self.pitchDisplay1['units'].setStyleSheet("font-size:20px; Arial")
-        self.pitchDisplay1['units'].setGeometry(280 + a, 180, 100, 100)
         #pitch lcd
         self.pitchDisplay1['lcd'] = QtWidgets.QLCDNumber(self)
-        self.pitchDisplay1['lcd'].setGeometry(150 + a, 200, 100, 50)
+        self.pitchDisplay1['lcd'].setGeometry(160, 235, 50, 30)
         #roll angle:
         self.rollDisplay1 = {}
-        #roll angle label
-        self.rollDisplay1['name'] = QtWidgets.QLabel(self)
-        self.rollDisplay1['name'].setText("Roll")
-        self.rollDisplay1['name'].setStyleSheet("font-size:20px; Arial")
-        self.rollDisplay1['name'].setGeometry(50+a, 250, 100,100)
-        #roll angle units label
-        self.rollDisplay1['units'] = QtWidgets.QLabel(self)
-        self.rollDisplay1['units'].setText("Deg")
-        self.rollDisplay1['units'].setStyleSheet("font-size:20px; Arial")
-        self.rollDisplay1['units'].setGeometry(280 +a , 250, 100,100)
+
         #roll lcd
         self.rollDisplay1['lcd'] = QtWidgets.QLCDNumber(self)
-        self.rollDisplay1['lcd'].setGeometry(150+a ,270, 100,50)
+        self.rollDisplay1['lcd'].setGeometry(220,235, 50,30)
 
 
 
         #----------------------------------
 
         #----------buttons config----------
+        #setting background start image
+        
+        self.start=QtWidgets.QLabel(self)
+        self.start.setGeometry(885,165,55,55)
+        Icon3=QtGui.QPixmap("gui/img/play.png")
+        Icon_resize3= Icon3.scaled(55,55,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.start.setPixmap(Icon_resize3)
+        
+        #setting background start image
+        
+        self.stop=QtWidgets.QLabel(self)
+        self.stop.setGeometry(885,235,55,55)
+        Icon4=QtGui.QPixmap("gui/img/stop.png")
+        Icon_resize5= Icon4.scaled(55,55,QtCore.Qt.KeepAspectRatio, transformMode=QtCore.Qt.SmoothTransformation)
+        self.stop.setPixmap(Icon_resize5)
+        
         self.controlButtons = {}
         #start button
-        self.controlButtons['start'] = QtWidgets.QPushButton(self)
-        self.controlButtons['start'].setText("Start")
-        self.controlButtons['start'].setGeometry(50,520,120,60)
-        self.controlButtons['start'].setStyleSheet("font-size:20px; Arial")
-        self.controlButtons['start'].setIcon(QtGui.QIcon('gui/img/play2'))
-        self.controlButtons['start'].setIconSize(QSize(40,40))
-        #stop button
-        self.controlButtons['stop'] = QtWidgets.QPushButton(self)
+        self.controlButtons['start'] = QtWidgets.QCommandLinkButton(self)
+        self.controlButtons['start'].setIconSize(QSize(0,0))
+        #self.controlButtons['start'].setText("Start")
+        self.controlButtons['start'].setGeometry(885,165,55,55)
+        
+        
+        self.controlButtons['stop'] = QtWidgets.QCommandLinkButton(self)
         self.controlButtons['stop'].setText("Stop")
-        self.controlButtons['stop'].setGeometry(180,520,120,60)
-        self.controlButtons['stop'].setStyleSheet("font-size:20px; Arial")
-        self.controlButtons['stop'].setIcon(QtGui.QIcon('gui/img/stop'))
-        self.controlButtons['stop'].setIconSize(QSize(40,40))
+        self.controlButtons['stop'].setGeometry(885,235,55,55)
+        #self.controlButtons['stop'].setIcon(QtGui.QIcon('gui/img/stop'))
+        self.controlButtons['stop'].setIconSize(QSize(0,0))
         self.controlButtons['stop'].setEnabled(False)
         #----------------------------------
         #
-        self.LabelPosture=QtWidgets.QLabel(self)
-        self.LabelPosture.setGeometry(760,60,220,440)
-        self.LabelPosture.setPixmap(QtGui.QPixmap('gui/img/cervical'))
+        #self.LabelPosture=QtWidgets.QLabel(self)
+        #self.LabelPosture.setGeometry(760,60,220,440)
+        #self.LabelPosture.setPixmap(QtGui.QPixmap('gui/img/cervical'))
         #
-        self.LabelTitle=QtWidgets.QLabel(self)
-        self.LabelTitle.setGeometry(788,10,240,30)
-        self.LabelTitle.setText("Posture Behavior")
-        self.LabelTitle.setStyleSheet("font-size:20px ;Arial")
+    
         #self.Stop.setIconSize(QSize(800,500))
         #self.LabelPosture1=QtWidgets.QLabel(self)
         #self.LabelPosture1.setGeometry(340,100,400,300)
@@ -275,15 +236,15 @@ class BorgButton(object):
         self.j = None
         #setting background Label Borgº
         self.Labelborg=QtWidgets.QLabel(self.window)
-        self.Labelborg.setGeometry(50,430,718,90)
-        Icon2=QtGui.QPixmap("gui/img/borg_act")
-        Icon_resize= Icon2.scaled(700,90)
+        self.Labelborg.setGeometry(25,465,730,55)
+        Icon2=QtGui.QPixmap("gui/img/borgh")
+        Icon_resize= Icon2.scaled(730,55)
         self.Labelborg.setPixmap(Icon_resize)
 
         #get borg scale buttons
         self.create_borg_button()
 
-    def create_borg_button(self, ep = 48,xp = 50, yp = 430, hp = 48,wp = 90):
+    def create_borg_button(self, ep = 48,xp = 30, yp = 473, hp = 39,wp = 39):
         self.Borg = []
         offset = 1
         x = xp
@@ -319,7 +280,7 @@ class BorgButton(object):
 
     def set_cursor(self, p):
         self.Borg[self.cursorStatus].setStyleSheet("border-style: outset")
-        self.Borg[p].setStyleSheet("border:3px solid rgb(0,0,0);")
+        self.Borg[p].setStyleSheet("border:2.5px solid rgb(50,50,50); border-radius:10px;")
         self.cursorStatus = p
 
     def move(self):
